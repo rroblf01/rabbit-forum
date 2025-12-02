221c49e1d84b03071cf093712832409252057131
Ricardo Robles <ricardo.r.f@hotmail.com>
2025-12-02 18:42:50 +0000

    Remove min_age field from Topic model and add migration

--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,7 @@
 from re import U
-from django.db import models
+
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Category(models.Model):
@@ -12,7 +13,6 @@ class Category(models.Model):
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
-    min_age = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
