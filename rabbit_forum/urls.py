4af718b6ac4f0fe83b17f69ea5d590b17b398007
Ricardo Robles <ricardo.r.f@hotmail.com>
2025-12-02 18:43:08 +0000

    Update URL configuration to include forum URLs and serve static files

--- rabbit_forum/urls.py
+++ rabbit_forum/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from forum.urls import urlpatterns as forum_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + forum_urls
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
